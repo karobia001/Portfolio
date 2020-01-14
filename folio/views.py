c1ba883997e5a7578629087d134e867a73c3d67a
karobia001 <karobiamaina81@gmail.com>
2020-01-14 17:56:18 +0300

    creates the view function that will handle the showing of my projects in the homepage

--- folio/views.py
+++ folio/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
+def projects_shown(request):
+    projects = Project.objects.all()
+    context = {
+        'projects':projects
+    }
+    return render(request, 'project_shown.html',context)
+
+
