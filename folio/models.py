3b8ba9cdc9a51fc1f55e510860f0d0e074720d33
karobia001 <karobiamaina81@gmail.com>
2020-01-23 16:58:34 +0300

    adds the requirments and all the other pages

--- folio/models.py
+++ folio/models.py
@@ -5,4 +5,14 @@ class Project(models.Model):
     title = models.CharField(max_length= 60)
     description = models.TextField()
     technology = models.CharField(max_length = 20)
-    image = models.FilePathField(path = "/img")
+    image= models.ImageField(blank = False)
+    giturl = models.TextField(max_length = 200 )
+    live = models.TextField(max_length = 200)
+
+    
+    
+@classmethod
+def get_all_projects(cls):
+        images = cls.objects.all()
+        return images
+    
--- folio/urls.py
+++ folio/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
-urlpatterns = [
-    path("", views.project_shown, name="project_shown"),
-    path("<int:pk>/", views.project_info, name="project_info"),
+urlpatterns=[
+path('', views.project_shown, name='project_shown'),
+path('<int:pk>/', views.project_info, name='project_info'),
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
